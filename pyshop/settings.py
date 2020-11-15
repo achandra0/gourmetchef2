9bdcb2745e838fb2195e735f1afd285aa2367d40
achandra0 <59938574+achandra0@users.noreply.github.com>
2020-11-14 20:01:34 -0600

    debug = False

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$m6xzszdrajay)9$p2kk1$2-lxs50)*6mi))lepqjw8p*-xax^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
