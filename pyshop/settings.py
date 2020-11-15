fb11f4e9caf19f18e324c1a82268b26f5056af27
Ananya Chandra <59938574+achandra0@users.noreply.github.com>
2020-11-14 20:39:55 -0600

    Update settings.py

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$m6xzszdrajay)9$p2kk1$2-lxs50)*6mi))lepqjw8p*-xax^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
